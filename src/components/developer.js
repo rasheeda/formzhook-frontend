0a5f52b794c5f2d87912228cf8d61daaf35fe8ec
Mandeeya <rashkeed@gmail.com>
2019-09-06 08:57:37 +0200

    Fixing issue with apiKey not getting saved as a token.

--- src/components/developer.js
+++ src/components/developer.js
@@ -1,17 +1,57 @@
 import React from "react";
-import { Popconfirm, message, Button, Row, Col, Divider } from "antd";
+import { Popconfirm, message, Button, Collapse, Table } from "antd";
 import { generateApiKey, getApiKey } from "./services";
 
 function confirm(e) {
-  message.success("Click on Yes");
   generateApiKey()
     .json(response => {
+      message.success("Key Generated");
+
       this.setState({
         apiKey: response.api_key
       });
+
+      localStorage.setItem("api_key", response.api_key);
+
+      window.location.reload();
     })
     .catch(() => {});
 }
+
+const API_KEY_COLUMNS = [
+  {
+    title: "API Key",
+    dataIndex: "api_key",
+    key: "api_key"
+  },
+  {
+    title: "Action",
+    dataIndex: "generate_btn",
+    key: "generate_btn",
+    render: generate_btn => (
+      <Popconfirm
+        title="Are you sure you want to generate a new key?"
+        onConfirm={confirm}
+        okText="Yes"
+        cancelText="No"
+      >
+        <Button type="primary" size="small">
+          Generate API Key
+        </Button>
+      </Popconfirm>
+    )
+  }
+];
+
+const apiKeyDataSource = [
+  {
+    key: "1",
+    api_key: localStorage.getItem("api_key")
+  }
+];
+
+const { Panel } = Collapse;
+
 
 class Developer extends React.Component {
   state = {
@@ -32,26 +72,26 @@ class Developer extends React.Component {
   render() {
     return (
       <div>
+        <Table dataSource={apiKeyDataSource} columns={API_KEY_COLUMNS} pagination={false} />
         <div>
-          <span>
-            {this.state.apiKey === ""
-              ? "No API Key Generated"
-              : this.state.apiKey}
-          </span>
-          <Divider type="vertical" />
-          <Popconfirm
-            title="Are you sure delete this generate a new key?"
-            onConfirm={confirm}
-            okText="Yes"
-            cancelText="No"
-          >
-            <Button type="primary" size="small">
-              Generate API Key
-            </Button>
-          </Popconfirm>
+          <h3>API Documentation</h3>
+          
+          <Collapse defaultActiveKey={["1"]}>
+            <Panel header="Post Data to A Form" key="1">
+              <p>To post data, the following parameters are required</p>
+              <p></p>
+              <p>
+                GET{" "}
+                <strong>
+                  <i>https://formzhook.io/api/formz/$form_unique_id/data</i>
+                </strong>
+              </p>
+            </Panel>
+            <Panel header="Get Form Data" key="2">
+              <p></p>
+            </Panel>
+          </Collapse>
         </div>
-        <Divider />
-        <div>Show API Documentation</div>
       </div>
     );
   }
